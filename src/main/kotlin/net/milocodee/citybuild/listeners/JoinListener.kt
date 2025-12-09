d3333f979c79b4497868b7d32411b4c392762d1c
milo <tegethoffm@icloud.com>
2025-12-09 19:07:42 +0100

    bug fix in JoinListener.kt

--- src/main/kotlin/net/milocodee/citybuild/listeners/JoinListener.kt
+++ src/main/kotlin/net/milocodee/citybuild/listeners/JoinListener.kt
@@ -1,13 +1,13 @@
 package net.milocodee.citybuild.listeners
 
-import net.milocodee.citybuild.CityBuildMain
+import net.milocodee.citybuild.plots.PlotManager
 import net.milocodee.citybuild.plots.data.PlotGeneration
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.player.PlayerJoinEvent
 
 
-class JoinListener(private val manager: CityBuildMain) : Listener {
+class JoinListener(private val manager: PlotManager) : Listener {
     @EventHandler
     fun onJoin(e: PlayerJoinEvent) {
         val player = e.getPlayer()
